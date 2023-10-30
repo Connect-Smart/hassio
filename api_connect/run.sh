535ff9332bf65304349b921a7d84c5a4e0d2282c
Jos Rothman <jos@rothman.nl>
2023-10-30 20:03:48 +0100

    new entity

--- api_connect/run.sh
+++ api_connect/run.sh
@@ -8,13 +8,38 @@ ENTITY_ID=$(jq --raw-output ".entity" $CONFIG_PATH)
 # Instellingen
 HA_HOST="http://localhost:8123"  # Vervang dit door het adres van jouw Home Assistant
 
-# De waarde die je wilt instellen
-NEW_VALUE="42"
-
-# API-aanroep om de entiteit bij te werken
-curl -X POST -H "Authorization: Bearer $HA_TOKEN" \
+EXISTING_ENTITY=$(curl -s -X GET -H "Authorization: Bearer $HA_TOKEN" \
      -H "Content-Type: application/json" \
-     -d "{\"state\": \"$NEW_VALUE\"}" \
-     "$HA_HOST/api/states/$ENTITY_ID"
+     "$HA_HOST/api/states/$ENTITY_ID")
 
-echo "Entiteit $ENTITY_ID bijgewerkt naar $NEW_VALUE"
+# Als de entiteit al bestaat, werk deze dan bij
+if [[ ! -z "$EXISTING_ENTITY" ]]; then
+    # De waarde die je wilt instellen
+    NEW_VALUE="42"
+
+    # API-aanroep om de entiteit bij te werken
+    curl -X POST -H "Authorization: Bearer $HA_TOKEN" \
+         -H "Content-Type: application/json" \
+         -d "{\"state\": \"$NEW_VALUE\"}" \
+         "$HA_HOST/api/states/$ENTITY_ID"
+
+    echo "Entiteit $ENTITY_ID bijgewerkt naar $NEW_VALUE"
+else
+    # De configuratie voor de nieuwe entiteit
+    ENTITY_CONFIG='{
+      "platform": "template",
+      "sensors": {
+        "example_sensor": {
+          "value_template": "{{ states.sensor.some_other_sensor.state }}"
+        }
+      }
+    }'
+
+    # API-aanroep om de configuratie toe te voegen
+    curl -X POST -H "Authorization: Bearer $HA_TOKEN" \
+         -H "Content-Type: application/json" \
+         -d "$ENTITY_CONFIG" \
+         "$HA_HOST/api/config/config_entries/entry_id/options"
+
+    echo "Nieuwe entiteit toegevoegd aan Home Assistant configuratie"
+fi
