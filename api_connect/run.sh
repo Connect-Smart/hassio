1fadb0f96c5208401ad0990ea556b58c43ee15dc
Jos Rothman <jos@rothman.nl>
2023-10-30 20:20:32 +0100

    updates

--- api_connect/run.sh
+++ api_connect/run.sh
@@ -4,6 +4,9 @@ CONFIG_PATH=/data/options.json
 
 HA_TOKEN=$(jq --raw-output ".token" $CONFIG_PATH)
 ENTITY_ID=$(jq --raw-output ".entity" $CONFIG_PATH)
+API_KEY=$(jq --raw-output ".apikey" $CONFIG_PATH)
+REMOTE_API_URL="https://www.voxip.nl/api/"
+INTERVAL=2  # Tijd in seconden tussen elk API-verzoek
 
 # Instellingen
 HA_HOST="http://localhost:8123"  # Vervang dit door het adres van jouw Home Assistant
@@ -15,7 +18,7 @@ EXISTING_ENTITY=$(curl -s -X GET -H "Authorization: Bearer $HA_TOKEN" \
 # Als de entiteit al bestaat, werk deze dan bij
 if [[ ! -z "$EXISTING_ENTITY" ]]; then
     # De waarde die je wilt instellen
-    NEW_VALUE="42"
+    NEW_VALUE="40"
 
     # API-aanroep om de entiteit bij te werken
     curl -X POST -H "Authorization: Bearer $HA_TOKEN" \
@@ -43,3 +46,25 @@ else
 
     echo "Nieuwe entiteit toegevoegd aan Home Assistant configuratie"
 fi
+
+perform_api_request() {
+    # Plaats hier je API-aanroep
+    # Bijvoorbeeld, een API-aanroep om de waarde van een entiteit op te halen
+     REMOTE_DATA=$(curl -s "$REMOTE_API_URL")
+
+    curl -X POST -H "Authorization: Bearer $HA_TOKEN" \
+         -H "Content-Type: application/json" \
+         -d "{\"state\": \"$REMOTE_DATA\"}" \
+         "$HA_HOST/api/states/$ENTITY_ID"
+
+    echo "Entiteit $ENTITY_ID bijgewerkt naar $REMOTE_DATA"
+}
+
+# Start de lus
+while true; do
+    # Voer de API-aanroep uit
+    perform_api_request
+
+    # Wacht het opgegeven interval voordat de lus opnieuw wordt uitgevoerd
+    sleep $INTERVAL
+done
