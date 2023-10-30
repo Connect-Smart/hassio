63489faf3271cc24774e4211f28416a427feffae
Jos Rothman <jos@rothman.nl>
2023-10-30 22:42:01 +0100

    update

--- api_connect/run.sh
+++ api_connect/run.sh
@@ -6,10 +6,10 @@ HA_TOKEN=$(jq --raw-output ".token" $CONFIG_PATH)
 ENTITY_ID=$(jq --raw-output ".entity" $CONFIG_PATH)
 API_KEY=$(jq --raw-output ".apikey" $CONFIG_PATH)
 
-#ENTITY_ID="sensor.$ENTITY"  # Vervang dit door het gewenste entiteits-ID
+ENTITY_ID="sensor.energie_prijzen_CS"  # Vervang dit door het gewenste entiteits-ID
 REMOTE_API_URL="https://www.voxip.nl/api/"
 INTERVAL=5  # Tijd in seconden tussen elk API-verzoek
-NEW_VALUE="20"
+NEW_VALUE="0"
 
 # Instellingen
 HA_HOST="http://localhost:8123"  # Vervang dit door het adres van jouw Home Assistant
@@ -19,7 +19,7 @@ curl -X POST -H "Authorization: Bearer $HA_TOKEN" \
      -d "{
        \"state\": \"29\",
        \"attributes\": {
-         \"friendly_name\": \"test123\"
+         \"friendly_name\": \"energie_prijzen\"
        }
      }" \
      "$HA_HOST/api/states/$ENTITY_ID"
