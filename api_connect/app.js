6e98412a8fe5ea36bb5a380d0d63073de578e0c8
Jos Rothman <jos@rothman.nl>
2023-10-31 23:35:25 +0100

    update

--- api_connect/app.js
+++ api_connect/app.js
@@ -1,45 +1,39 @@
-const http = require('http');
-const WebSocket = require('ws');
-const homeassistant = require('homeassistant-api'); // Vereist 'homeassistant-api' of een soortgelijke bibliotheek
+const express = require('express');
+const WebSocket = require('home-assistant-js-websocket');
+const app = express();
 
-// Verbinding maken met Home Assistant
-const ha = homeassistant({
-  host: 'http://homeassistant.local',
-  token: 'YOUR_LONG_LIVED_ACCESS_TOKEN',
+// Verander dit naar het IP-adres en het wachtwoord van je Home Assistant-installatie.
+const haUrl = 'http://10.0.0.20:8123';
+const haAuthToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0MmJiYzgzOTA3YTQ0NjgyYTIwMDU3YTg0MWM4ZDZmMSIsImlhdCI6MTY5ODc5MTcxMSwiZXhwIjoyMDE0MTUxNzExfQ.AgCMiPY7svqVC3fs_PxiePBFWtEg6cQYG-eISc1q18Y';
+
+const haSocket = new WebSocket.Connection({
+  createSocketUrl: (path) => `${haUrl}/api/websocket`,
+  accessToken: haAuthToken,
 });
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Dit is een Home Assistant add-on met WebSocket-ondersteuning!\n');
+haSocket.connect();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Status van de entiteit</h1>
+    <p id="entityStatus">Laden...</p>
+    <script>
+      const entityStatus = document.getElementById('entityStatus');
+      const socket = new WebSocket('${haUrl}/api/websocket');
+      socket.onopen = () => {
+        socket.send(JSON.stringify({ type: 'auth', access_token: '${haAuthToken}' }));
+        socket.send(JSON.stringify({ id: 1, type: 'subscribe_events', event_type: 'state_changed' }));
+      };
+      socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.type === 'event' && data.event.event_type === 'state_changed') {
+          entityStatus.innerText = 'Status: ' + data.event.data.new_state.state;
+        }
+      };
+    </script>
+  `);
 });
 
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws) => {
-  // Wanneer een client een WebSocket-verbinding tot stand brengt
-
-  // Abonneer je op updates van een specifieke entiteit (bijv. een licht)
-  const entityId = 'light.living_room';
-  ha.subscribeState(entityId, (newState) => {
-    // Verstuur de nieuwe entiteitstoestand naar de WebSocket-client
-    ws.send(JSON.stringify({ entity: entityId, state: newState }));
-  });
-
-  ws.on('close', () => {
-    // Wanneer de client de WebSocket-verbinding sluit, stop de entiteitssubscriptie
-    ha.unsubscribeState(entityId);
-  });
-});
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Server draait op poort ${port}`);
-});
-
-process.on('SIGTERM', () => {
-  console.log('Ontvangen SIGTERM, server wordt afgesloten...');
-  server.close(() => {
-    console.log('Server is gestopt.');
-  });
+app.listen(8099, () => {
+  console.log('Server gestart op poort 8099');
 });
