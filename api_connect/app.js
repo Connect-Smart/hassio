044890a3a8aeadc51a786eab59cd0e5767ffcad0
Jos Rothman <jos@rothman.nl>
2023-10-31 22:22:10 +0100

    test update

---
+++ api_connect/app.js
@@ -0,0 +1,55 @@
+const WebSocket = require("ws");
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const html = fs.readFileSync("www/index.html", "utf-8");
+    res.end(html);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+const wss = new WebSocket.Server({ noServer: true });
+
+wss.on("connection", (ws) => {
+  console.log("WebSocket client connected");
+
+  // Simuleer periodieke updates
+  const updateInterval = setInterval(() => {
+    const entityName = "New Sensor";
+    const entityValue = Math.floor(Math.random() * 100);
+    ws.send(
+      JSON.stringify({
+        event_type: "state_changed",
+        data: {
+          entity_id: "sensor.new_sensor",
+          new_state: {
+            attributes: {
+              friendly_name: entityName,
+            },
+            state: entityValue.toString(),
+          },
+        },
+      })
+    );
+  }, 5000);
+
+  ws.on("close", () => {
+    console.log("WebSocket client disconnected");
+    clearInterval(updateInterval);
+  });
+});
+
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit("connection", ws, request);
+  });
+});
+
+server.listen(8099, () => {
+  console.log("Web server is running on port 8099");
+});
