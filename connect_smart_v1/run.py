136fe967acc01088b3630385ee332fe4b7cee4aa
VoxIP <jos@voxip.nl>
2023-12-06 14:31:26 +0100

    Update run.py

--- connect_smart_v1/run.py
+++ connect_smart_v1/run.py
@@ -1,9 +1,7 @@
 import os
 import requests
 from flask import Flask, jsonify
-from datetime import datetime, time
-import json
-from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -20,49 +18,65 @@ headers = {
     "Content-Type": "application/json",
 }
 
-# Variabelen om de goedkoopste en duurste tijd op te slaan
-goedkoopste_tijd = None
-duurste_tijd = None
-
-def haal_en_verwerk_tijdinformatie():
+def fetch_energy_data():
     login_data = {"username": API_USERNAME, "password": API_PASSWORD}
     website_data_url = "https://voxip.nl/api"
     response = requests.get(website_data_url, data=login_data)
-    
+
     if response.ok:
-        # Verwerk de JSON en sla de tijden op
-        data = response.json()
-        global goedkoopste_tijd, duurste_tijd
-        goedkoopste_tijd = datetime.strptime(data["goedkoopste_tijd"], "%H:%M:%S").time()
-        duurste_tijd = datetime.strptime(data["duurste_tijd"], "%H:%M:%S").time()
+        return response.json()
     else:
-        print("Fout bij het ophalen van tijdinformatie")
+        return None
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(haal_en_verwerk_tijdinformatie, 'interval', minutes=30)
-scheduler.start()
+def extract_times(energy_data):
+    cheapest_time = energy_data.get("cheapest_time")
+    most_expensive_time = energy_data.get("most_expensive_time")
 
-@app.route('/toggle-switch', methods=['POST'])
-def toggle_switch():
-    entity_id = "switch.jouw_switch_entity_id"
-    url = f"{HASS_API}/services/switch/toggle"
-    data = {"entity_id": entity_id}
-    
+    return cheapest_time, most_expensive_time
+
+def save_times_to_home_assistant(cheapest_time, most_expensive_time):
+    cheapest_entity_id = "sensor.cheapest_energy_time"
+    expensive_entity_id = "sensor.expensive_energy_time"
+
+    # Format times as HH:MM
+    cheapest_time_str = datetime.strptime(cheapest_time, "%Y-%m-%dT%H:%M:%S").strftime("%H:%M")
+    most_expensive_time_str = datetime.strptime(most_expensive_time, "%Y-%m-%dT%H:%M:%S").strftime("%H:%M")
+
+    # Update entities in Home Assistant
+    update_entity(cheapest_entity_id, cheapest_time_str)
+    update_entity(expensive_entity_id, most_expensive_time_str)
+
+def update_entity(entity_id, state):
+    url = f"{HASS_API}/states/{entity_id}"
+    data = {"state": state}
     response = requests.post(url, headers=headers, json=data)
-    
-    if response.ok:
-        return "Switch toggled!", 200
-    else:
-        return "Failed to toggle switch.", 500
+    return response.ok
+
+def create_automation(cheapest_time, most_expensive_time):
+    automation_entity_id = "SC_energy_scheduler"
+
+    automation_data = {
+        "entity_id": automation_entity_id,
+        "at": [cheapest_time, most_expensive_time]
+    }
+
+    url = f"{HASS_API}/services/automation/trigger"
+    response = requests.post(url, headers=headers, json=automation_data)
+
+    return response.ok
 
 @app.route('/admin', methods=['GET'])
-def get_test_sensor():
-    # Hier kun je de goedkoopste en duurste tijdgegevens retourneren
-    return jsonify({"goedkoopste_tijd": str(goedkoopste_tijd), "duurste_tijd": str(duurste_tijd)}), 200
+def get_energy_data():
+    energy_data = fetch_energy_data()
+
+    if energy_data:
+        cheapest_time, most_expensive_time = extract_times(energy_data)
+        save_times_to_home_assistant(cheapest_time, most_expensive_time)
+        create_automation(cheapest_time, most_expensive_time)
+        return "Data updated successfully.", 200
+    else:
+        return "Failed to fetch energy data.", 500
 
 if __name__ == '__main__':
-    # Voer de functie een keer uit om de initiële tijdinformatie op te halen
-    haal_en_verwerk_tijdinformatie()
-    
-    # Start de Flask-applicatie
     app.run(host='0.0.0.0', port=8080)
+
