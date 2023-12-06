5977dba1ba87a526b1b40e1726ba0e07655ac7de
VoxIP <jos@voxip.nl>
2023-12-06 12:58:42 +0100

    Update run.py

--- connect_smart_v1/run.py
+++ connect_smart_v1/run.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from flask import Flask, jsonify
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -20,7 +21,7 @@ headers = {
 def login_to_website(username, password):
     # Hier implementeer je de logica om in te loggen op de website
     # Vervang de onderstaande code door de echte inloglogica van de website
-    login_url = "https://voxip.nl/api"
+    login_url = "https://voorbeeld-website.com/login"
     login_data = {"username": username, "password": password}
 
     response = requests.post(login_url, data=login_data)
@@ -29,6 +30,21 @@ def login_to_website(username, password):
         return True
     else:
         return False
+
+def get_website_data():
+    # Hier implementeer je de logica om gegevens van de website te halen
+    # Vervang de onderstaande code door de echte logica om gegevens van de website te halen
+    website_url = "https://voxip.nl/api"
+    
+    # In dit voorbeeld gebruiken we requests en BeautifulSoup voor webscraping
+    response = requests.get(website_url)
+
+    if response.ok:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        value = float(soup.find(id='value').text)
+        return {"value": value}
+    else:
+        return None
 
 @app.route('/toggle-switch', methods=['POST'])
 def toggle_switch():
@@ -49,20 +65,21 @@ def get_test_sensor():
     
     # Inloggen op de website
     if login_to_website(WEBSITE_USERNAME, WEBSITE_PASSWORD):
-        # Hier implementeer je de logica om gegevens van de website te halen
-        # Vervang de onderstaande code door de echte logica om gegevens van de website te halen
-        website_data = {"temperature": 25.0, "humidity": 50.0}
+        # Gegevens van de website ophalen
+        website_data = get_website_data()
 
-        # Bijwerken van de test sensor met gegevens van de website
-        update_url = f"{HASS_API}/states/{entity_id}"
-        update_data = website_data
+        if website_data is not None:
+            update_url = f"{HASS_API}/states/{entity_id}"
+            update_data = website_data
 
-        response = requests.post(update_url, headers=headers, json=update_data)
+            response = requests.post(update_url, headers=headers, json=update_data)
 
-        if response.ok:
-            return "Test sensor updated!", 200
+            if response.ok:
+                return "Test sensor updated!", 200
+            else:
+                return "Failed to update test sensor.", 500
         else:
-            return "Failed to update test sensor.", 500
+            return "Failed to retrieve data from the website.", 500
     else:
         return "Failed to login to the website.", 500
 
