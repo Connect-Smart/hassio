2bfc89cad9b14964781e06f4e7951346763c7d1b
VoxIP <jos@voxip.nl>
2024-01-10 18:50:25 +0100

    Update run.py

--- connect_smart_v1/run.py
+++ connect_smart_v1/run.py
@@ -102,7 +102,6 @@ def admin_panel():
     return render_template('admin_panel.html')  # Maak een HTML-sjabloon voor je admin-paneel
 
 @app.route('/admin_panel', methods=['GET'])
-def admin_panel():
     return render_template('toggle_switch.html')
 
 @app.route('/energy_data', methods=['GET'])
